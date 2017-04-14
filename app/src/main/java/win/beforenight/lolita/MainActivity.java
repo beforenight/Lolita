9374383f82754a04b31a9d0d0ed8820287428c18
zhangswings <771068383@qq.com>
2017-04-14 11:15:44 +0800

    +初始化项目

---
+++ app/src/main/java/win/beforenight/lolita/MainActivity.java
@@ -0,0 +1,73 @@
+package win.beforenight.lolita;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+{
+    private static final String TAG = "WebData";
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener()
+    {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item)
+        {
+            switch (item.getItemId())
+            {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    private void initIntentData()
+    {
+        Intent intent = getIntent();
+        if (intent != null)
+        {
+            String intentAction = intent.getAction();
+            if (Intent.ACTION_VIEW.equals(intentAction))
+            {
+                Uri intentData = intent.getData();
+                String name = intentData.getQueryParameter("name");
+                String page = intentData.getQueryParameter("page");
+                Log.e(TAG, "initIntentData: " + name);
+                Log.e(TAG, "initIntentData: " + page);
+
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        initIntentData();
+    }
+
+}
