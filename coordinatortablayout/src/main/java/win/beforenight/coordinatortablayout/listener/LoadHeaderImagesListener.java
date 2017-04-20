c641e9b7ab508138931812eb88391a373118a274
zhangswings <771068383@qq.com>
2017-04-20 14:05:03 +0800

    +Combination of TabLayout and CoordinatorLayout./TabLayout和CoordinatorLayout相结合的折叠控件 +fork from https://github.com/hugeterry/CoordinatorTabLayout

---
+++ coordinatortablayout/src/main/java/win/beforenight/coordinatortablayout/listener/LoadHeaderImagesListener.java
@@ -0,0 +1,13 @@
+package win.beforenight.coordinatortablayout.listener;
+
+import android.support.design.widget.TabLayout;
+import android.widget.ImageView;
+
+/**
+ * @author hugeterry(http://hugeterry.cn)
+ */
+
+public interface LoadHeaderImagesListener
+{
+    void loadHeaderImages(ImageView imageView, TabLayout.Tab tab);
+}
